4a7cd6286b90c9356eda1bbe2bde7fd8693bca57
DanielaChokan <chokandaniela34@gmail.com>
2026-05-02 20:19:14 +0300

    Add footer and header components with styles; update global styles and fonts

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.top}>
+        <div className={styles.brand}>
+          <div className={styles.logo}>
+            <Image
+              src="/icons/header-logo.png"
+              alt="FloraSwift"
+              width={44}
+              height={44}
+              className={styles.logoImage}
+            />
+            <span>FloraSwift</span>
+          </div>
+          <div className={styles.contacts}>
+            <div className={styles.contactRow}>
+              <Image src="/icons/footer-location.png" alt="" width={16} height={16} />
+              <span>м. Суми вул. Ковпака 4</span>
+            </div>
+            <div className={styles.contactRow}>
+              <Image src="/icons/footer-call.png" alt="" width={16} height={16} />
+              <span>+380 99 000 0000</span>
+            </div>
+          </div>
+          <button type="button" className={styles.callButton}>
+            Замовити дзвінок
+          </button>
+        </div>
+
+        <div className={styles.centerCol}>
+          <ul className={styles.linkList}>
+            <li>Статті</li>
+            <li>Юр. особам</li>
+            <li>Допомога</li>
+            <li>Доставка</li>
+          </ul>
+          <button type="button" className={styles.catalogButton}>
+            Каталог
+          </button>
+        </div>
+
+        <div className={styles.rightCol}>
+          <ul className={styles.linkList}>
+            <li>Оплата</li>
+            <li>Умови повернення</li>
+            <li>Робота у нас</li>
+            <li>Політика конфіденційності</li>
+          </ul>
+          <div className={styles.social}>
+            <span>Ми в соцмережах</span>
+            <div className={styles.socialIcons}>
+              <Image src="/icons/footer-pinterest.png" alt="Pinterest" width={24} height={24} />
+              <Image src="/icons/footer-telegram.png" alt="Telegram" width={24} height={24} />
+              <Image src="/icons/footer-tik-tok.png" alt="TikTok" width={24} height={24} />
+              <Image src="/icons/footer-instagram.png" alt="Instagram" width={24} height={24} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.bottom}>© Copyright 2026</div>
+    </footer>
+  );
+}
---
+++ components/layout/Header.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Image
+          src="/icons/header-logo.png"
+          alt="FloraSwift"
+          width={44}
+          height={44}
+        />
+        <span>FloraSwift</span>
+      </div>
+      <p className={styles.centerText}>Доставка свіжих квітів 24/7</p>
+      <div className={styles.actions}>
+        <button type="button" className={styles.action}>
+          <Image src="/icons/header-search.png" alt="Пошук" width={22} height={22} />
+          <span>Пошук</span>
+        </button>
+        <button type="button" className={styles.action}>
+          <Image
+            src="/icons/header-favourite.png"
+            alt="Обране"
+            width={22}
+            height={22}
+          />
+          <span>Обране</span>
+        </button>
+        <button type="button" className={styles.action}>
+          <Image src="/icons/header-cart.png" alt="Кошик" width={22} height={22} />
+          <span>Кошик</span>
+        </button>
+                <button type="button" className={styles.action}>
+          <Image
+            src="/icons/header-profile.png"
+            alt="Вхід"
+            width={22}
+            height={22}
+          />
+          <span>Вхід</span>
+        </button>
+      </div>
+    </header>
+  );
+}
